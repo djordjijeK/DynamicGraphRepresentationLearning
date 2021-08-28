622ce53c72f37316b04dd6cedff5c14189d52612
spapadias <s.papadias@protonmail.com>
2021-08-28 12:46:03 +0300

    self-speedup script/src

---
+++ experiments/scripts/self-speedup.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# script options
+clean_build=True                                    # cleans build folder after the execution
+
+# execution options
+walk_model="deepwalk"                               # deepwalk | node2vec
+paramP=0.5                                          # node2vec's paramP
+paramQ=2.0                                          # node2vec's paramQ
+sampler_init_strategy="random"                      # random | burnin | weight
+declare -a graphs=("youtube-graph") # "livejournal-graph" "orkut-graph") # array of graphs
+declare -a walks_per_vertex=(10)                    # walks per vertex to generate
+declare -a walk_length=(80)                         # length of one walk
+#declare -a sizes=(1 2 3 4 5 6 7 8 9 10 11 12)       # exponent of the head frequency
+range_search="true"                                 # range search mode
+determinism="true"                                  # determinism
+# todo: Manually set the SERIAL OPTION to TRUE before running
+parallelism="parallel"                                # serial | parallel
+
+# create the data dir
+mkdir -p data/self-speedup/
+
+# 1. convert graphs in adjacency graph format if necessary
+for graph in "${graphs[@]}"; do
+  FILE=../data/"${graph}".adj
+  if test -f "$FILE"; then
+      echo 'Skipping conversion of a graph ' "${graph[@]}" ' to adjacency graph format!'
+  else
+      echo 'Converting graph ' "${graph[@]}" ' to adjacency graph format ...'
+      ./../bin/SNAPtoAdj -s -f "../data/${graph}" "../data/${graph}.adj"
+      echo 'Graph ' "${graph[@]}" ' converted to adjacency graph format!'
+  fi
+done
+
+# 2. build the executable
+mkdir -p ../../build;
+cd ../../build;
+cmake -DCMAKE_BUILD_TYPE=Release ..;
+cd experiments;
+make self-speedup
+
+# 3. execute experiments
+for wpv in "${walks_per_vertex[@]}"; do
+    for wl in "${walk_length[@]}"; do
+        for graph in "${graphs[@]}"; do
+            for hf in "${sizes[@]}"; do
+                printf "\n"
+                printf "Graph: ${graph}\n"
+                ./self-speedup -s -f "data/${graph}.adj" -w "${wpv}" -l "${wl}" -model "${walk_model}" -paramP "${paramP}" -paramQ "${paramQ}" -init "${sampler_init_strategy}" -rs "${range_search}" -d "${determinism}" -p "${parallelism}" | tee data/self_speedup/${graph}-${parallelism}.dat
+            done
+        done
+    done
+done
+
+# 4. clean build if necessary
+if [ "$clean_build" = True ] ; then
+    cd ../../;
+    rm -rf build;
+    # rm experiments/data/*.adj
+fi
---
+++ experiments/src/self_speedup.cpp
@@ -0,0 +1,135 @@
+#include <malin.h>
+
+void self_speedup(commandLine& command_line)
+{
+    string fname             = string(command_line.getOptionValue("-f", default_file_name));
+    bool mmap                = command_line.getOption("-m");
+    bool is_symmetric        = command_line.getOption("-s");
+    bool compressed          = command_line.getOption("-c");
+    size_t n_parts           = command_line.getOptionLongValue("-nparts", 1);
+
+    size_t walks_per_vertex = command_line.getOptionLongValue("-w", config::walks_per_vertex);
+    size_t length_of_walks  = command_line.getOptionLongValue("-l", config::walk_length);
+    string model            = string(command_line.getOptionValue("-model", "deepwalk"));
+    double paramP           = command_line.getOptionDoubleValue("-paramP", config::paramP);
+    double paramQ           = command_line.getOptionDoubleValue("-paramQ", config::paramQ);
+    string init_strategy    = string(command_line.getOptionValue("-init", "weight"));
+
+    string determinism      = string(command_line.getOptionValue("-d", "false"));
+    string range_search     = string(command_line.getOptionValue("-rs", "true"));
+//    int head_frequency      = command_line.getOptionIntValue("-hf", 8); // chunk size is 2^8 = 256 (slightly bigger than 128. aspen's machine cachelinesizes)
+    string parallelism      = string(command_line.getOptionValue("-p", "parallel"));
+
+    config::walks_per_vertex = walks_per_vertex;
+    config::walk_length      = length_of_walks;
+
+    std::cout << "Walks per vertex: " << (int) config::walks_per_vertex << std::endl;
+    std::cout << "Walk length: " << (int) config::walk_length << std::endl;
+
+    if (model == "deepwalk")
+    {
+        config::random_walk_model = types::RandomWalkModelType::DEEPWALK;
+
+        std::cout << "Walking model: DEEPWALK" << std::endl;
+    }
+    else if (model == "node2vec")
+    {
+        config::random_walk_model = types::RandomWalkModelType::NODE2VEC;
+        config::paramP = paramP;
+        config::paramQ = paramQ;
+
+        std::cout << "Walking model: NODE2VEC | Params (p,q) = " << "(" << config::paramP << "," << config::paramQ << ")" << std::endl;
+    }
+    else
+    {
+        std::cerr << "Unrecognized walking model! Abort" << std::endl;
+        std::exit(1);
+    }
+
+    if (init_strategy == "burnin")
+    {
+        config::sampler_init_strategy = types::SamplerInitStartegy::BURNIN;
+
+        std::cout << "Sampler strategy: BURNIN" << std::endl;
+    }
+    else if (init_strategy == "weight")
+    {
+        config::sampler_init_strategy = types::SamplerInitStartegy::WEIGHT;
+
+        std::cout << "Sampler strategy: WEIGHT" << std::endl;
+    }
+    else if (init_strategy == "random")
+    {
+        config::sampler_init_strategy = types::SamplerInitStartegy::RANDOM;
+
+        std::cout << "Sampler strategy: RANDOM" << std::endl;
+    }
+    else
+    {
+        std::cerr << "Unrecognized sampler init strategy" << std::endl;
+        std::exit(1);
+    }
+
+    // Set up the range search mode ------
+    if (range_search == "true")
+        config::range_search_mode = true;
+    else
+        config::range_search_mode = false;
+
+    // Set up the deterministic mode
+    if (determinism == "true")
+        config::deterministic_mode = true;
+    else
+        config::deterministic_mode = false;
+    // ------------------------------------
+
+    if (parallelism == "parallel")
+        cout << "PARALLEL MODE (48 THREADS)" << endl;
+    else if (parallelism == "serial")
+        cout << "SERIAL MODE (1 THREAD)" << endl;
+
+    // Assign the head frequency we read
+//    compressed_lists::head_frequency = (size_t) head_frequency;
+    cout << endl << "*** Head frequency is " << compressed_lists::head_frequency << ", and thus, chunk size is " << compressed_lists::head_mask << endl;
+    // ---------------------------------
+
+    size_t n;
+    size_t m;
+    uintE* offsets;
+    uintV* edges;
+    std::tie(n, m, offsets, edges) = read_unweighted_graph(fname.c_str(), is_symmetric, mmap);
+
+    dygrl::Malin malin = dygrl::Malin(n, m, offsets, edges);
+
+    auto produce_initial_walk_corpus = timer("WalkCorpusProduction", false); produce_initial_walk_corpus.start();
+    // Produce the initial random walk corpus
+    malin.generate_initial_random_walks();
+    auto walk_corpus_production_time =  produce_initial_walk_corpus.get_total();
+
+    // Print the memory requirements to store the produced walk corpus
+    malin.memory_footprint();
+
+//    auto traverse_walk_corpus = timer("WalkCorpusTraversal", false); traverse_walk_corpus.start();
+//    // print random walks
+//    cout << "k=" << compressed_lists::head_frequency << " walk corpus traversal started..." << endl;
+//    for(int i = 0; i < config::walks_per_vertex * malin.number_of_vertices(); i++)
+//        malin.traverse_walk(i) ;
+//    auto walk_corpus_traversal_time = traverse_walk_corpus.get_total();
+//    cout << "k=" << compressed_lists::head_frequency << " walk corpus traversal finished..." << endl;
+
+    cout << "Time to produce the walk  corpus: " << walk_corpus_production_time << endl;
+//         << "Time to traverse the walk corpus: " << walk_corpus_traversal_time  << endl;
+
+}
+
+
+int main(int argc, char** argv)
+{
+    std::cout << "Running experiment with: " << num_workers() << " threads." << std::endl;
+    commandLine command_line(argc, argv, "");
+
+    self_speedup(command_line);
+}
+
+
+
