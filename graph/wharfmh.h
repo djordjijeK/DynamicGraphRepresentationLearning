baec14bc7af14d024469285ffdbe747d100f9c4a
spapadias <s.papadias@protonmail.com>
2022-06-28 12:30:42 +0200

    cout #walks generated.

--- graph/wharfmh.h
+++ graph/wharfmh.h
@@ -221,6 +221,7 @@ namespace dynamic_graph_representation_learning_with_metropolis_hastings
                 auto graph            = this->flatten_graph();
                 auto total_vertices = this->number_of_vertices();
                 auto walks          = total_vertices * config::walks_per_vertex;
+				cout << "GENERATING " << walks << " random walks." << endl;
 //                auto cuckoo         = libcuckoo::cuckoohash_map<types::Vertex, std::vector<types::Vertex>>(total_vertices);
 
                 using VertexStruct  = std::pair<types::Vertex, VertexEntry>;
